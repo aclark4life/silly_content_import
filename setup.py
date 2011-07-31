b2c8dd243f2e819724d749c6a98711d5422dffe2
Alex Clark <aclark@aclark.net>
2011-07-30 20:06:19 -0400

    Make it work

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+    name='migrate',
+    py_modules=['migrate'],
+    install_requires=[
+        'plone.app.transmogrifier',
+        'transmogrify.filesystem',
+    ]
+)
